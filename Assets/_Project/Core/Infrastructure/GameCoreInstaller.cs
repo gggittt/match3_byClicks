319438394f1d07ace4132e4b46cbb0f2a97bc686
gggitttLocal <rsepmee@gmail.com>
2024-09-25 21:10:38 +0300

    added zenject, doTween, TMP, Odin

---
+++ Assets/_Project/Core/Infrastructure/GameCoreInstaller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace _Project.Core.Infrastructure
+{
+public class GameCoreInstaller : MonoInstaller
+{
+    // [SerializeField] Player _player;
+    // [Space]
+    // [SerializeField] Board _board;
+
+
+    public override void InstallBindings( )
+    {
+
+    }
+}
+
+}
